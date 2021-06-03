9773abd481f3617177a2099a2f7c12f8f88c4fae
Picbridge <34910988+Picbridge@users.noreply.github.com>
2021-06-02 22:47:38 -0700

    Switch_Projects

---
+++ Project4/Assets/Script_BasicControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_BasicControl : MonoBehaviour
+{
+    public float speed;
+    Vector3 movement;
+    // Use this for initialization
+    void Start()
+    {
+        //Hide the curser and lock the cursor inside the game
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxisRaw("Vertical");
+        float straffe = Input.GetAxisRaw("Horizontal");
+
+        movement = new Vector3(-translation, 0, straffe);
+
+        if (movement != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(movement);
+
+        //translation *= Time.deltaTime;
+        //straffe *= Time.deltaTime;
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
